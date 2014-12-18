acf4074ce7ea36f90a515f24e032e55f1c6df119
Alex Vandiver <alexmv@bestpractical.com>
2014-12-18 16:46:23 -0500

    Use standard AUTHOR, BUGS, and LICENSE AND COPYRIGHT blocks

--- lib/RT/Extension/MergeUsers.pm
+++ lib/RT/Extension/MergeUsers.pm
@@ -460,14 +460,27 @@ sub SetDisabled {
     return ($ret, $msg);
 }
 
-
 =head1 AUTHOR
 
-Alex Vandiver E<lt>alexmv@bestpractical.comE<gt>
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
 
-=head1 LICENSE
+=head1 BUGS
 
-GPL version 2.
+All bugs should be reported via email to
+
+    L<bug-RT-Extension-MergeUsers@rt.cpan.org|mailto:bug-RT-Extension-MergeUsers@rt.cpan.org>
+
+or via the web at
+
+    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-MergeUsers>.
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2014 by Best Practical Solutions
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
 
 =cut
 
