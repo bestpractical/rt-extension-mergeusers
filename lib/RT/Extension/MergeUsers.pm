b4b9e65dcefc0250239f69fc861e23bf25bfac2d
craig kaiser <craig@bestpractical.com>
2020-07-09 16:47:26 -0400

    Update POD to reference RT5

--- lib/RT/Extension/MergeUsers.pm
+++ lib/RT/Extension/MergeUsers.pm
@@ -63,7 +63,7 @@ RT::Extension::MergeUsers - Merges two users into the same effective user
  
 =head1 RT VERSION
 
-Works with RT 4.0, 4.2 and 4.4.
+Works with RT 4.0, 4.2, 4.4, 5.0.
 
 =head1 DESCRIPTION
 
@@ -92,7 +92,7 @@ Be sure to also read L</UPGRADING> if you are upgrading.
 
 May need root permissions
 
-=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
 
 If you are using RT 4.2 or greater, add this line:
 
@@ -106,7 +106,7 @@ or add C<RT::Extension::MergeUsers> to your existing C<@Plugins> line.
 
 =item Clear your mason cache
 
-    rm -rf /opt/rt4/var/mason_data/obj
+    rm -rf /opt/rt5/var/mason_data/obj
 
 =item Restart your webserver
 
